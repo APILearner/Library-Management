539d5043e90f80e394dfbe7aa82f763a5f8d97d8
Shashanka <tech.sasank@gmail.com>
2021-08-25 13:04:33 +0530

    First commit

---
+++ src/test/java/com/barclay/bank/LibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barclay.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
